d3fa92db685cde4d8d021b00051416e5a30fbd5a
papasv <stelios05@gmail.com>
2012-05-19 23:16:07 +0100

    still buggy

--- 95/main.cc
+++ 95/main.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 typedef unsigned long number_t;
 
-number_t get_root_or_bigger(number_t target){
+number_t get_bigger_than_root(number_t target){
   number_t low=2;
   number_t high=target;
   while(low<high){
@@ -19,16 +19,14 @@ number_t get_root_or_bigger(number_t target){
       }
       low=mid;
     }else{
-      return mid;
+      return mid+1;
     }
   }
   return high;
 }
-
-
 number_t sum_divisors(number_t target){
   number_t sum=1;
-  number_t max=get_root_or_bigger(target);
+  number_t max=min(get_bigger_than_root(target),target);
   for(number_t i=2;i<max;i++){
     if(target%i==0){
       sum+=i;
@@ -42,30 +40,79 @@ number_t sum_divisors(number_t target){
   return sum;
 }
 
+
 number_t max1=1000000;
-deque<pair<number_t,int> > chain_links(max1+1,pair<number_t,int>(0,0));
+struct data_t{
+  number_t next;
+  int id;
+  data_t()
+    :next(0),
+     id(0){}
+};
+deque<data_t > chain_links(max1+1);
+long chain_id=0;
+
+number_t smallest_ever=max1;
+size_t longest=0;
+void count_loop_size(number_t start){
+  size_t count=1;
+  number_t smallest=start;
+  for(number_t i = chain_links[start].next;i!=start;++count,i=chain_links[i].next){
+    cout<<"count_loop_size "<<start<<" "<<i<<endl;
+    smallest=min(smallest,i);
+  }
+  if(longest<=count){
+    smallest_ever=min(smallest_ever,smallest);
+  }
+}
+
+
+void detect_loop(number_t point){
+  chain_id++;
+  do{
+    data_t& data=chain_links[point];
+    if(data.id!=0){
+      if(data.id==chain_id){
+	//found loop
+	count_loop_size(point);
+	return;
+      }else{
+	//oops an other chain
+	return;
+      }
+    }
+    data.id=chain_id;
+  }while(true);
+} 
 void follow_chain(number_t point){
   while(true){
+    cout<<"follow_chain "<<point<<endl;
     if(point>max1){
       return;
     }
-    if(chain_links[point].first==0){
+    data_t& data=chain_links[point];
+    if(data.next==0){
       //new link
-      point=chain_links[point].first=sum_divisors(point);
+      point=data.next=sum_divisors(point);
     } else {
-      cout<<"found loop "<<point<<" "<<chain_links[point].first<<endl;
-      exit(0);
+      //cout<<"found loop "<<point<<" "<<chain_links[point].first<<endl;
+      //exit(0);
+      detect_loop(point);
+      return;
     }
   }
-    
 }
 int main(){
   /*number_t data[]={12496 , 14288 , 15472 , 14536 , 14264};
   for(int i=0;i<5;i++){
     cout<<data[i]<<" "<<sum_divisors(data[i])<<endl;
     }*/
-  for(number_t i = 2;i<=max1;i++){
+  //cout<<4<<" "<<sum_divisors(4)<<endl;
+  //return 0;
+  for(number_t i = 1;i<=max1;i++){
+    cout<<i<<endl;
     follow_chain(i);
   }
+  cout<<smallest_ever<<endl;
   return 0;
 }
