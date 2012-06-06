b200f67aa1a773aa087196c1bc1a9aa155f406fb
papasv <stelios05@gmail.com>
2012-06-06 23:00:17 +0100

    checkpoint

--- 96/sudoku.h
+++ 96/sudoku.h
@@ -2,6 +2,7 @@
 #define SUDOKU_HH SUDOKU_HH
 #include <string>
 #include <vector>
+#include <iostream>
 class sudoku{
   struct empty{
     size_t location;
@@ -12,18 +13,30 @@ class sudoku{
   sudoku(const std::string& original)
     :board(original){
   }
+  void exclude(char c,std::string& str){
+    size_t found=str.find(c);
+    if(found!=std::string::npos){
+      //std::cout<<"removing "<<c<<" from "<<str<<std::endl;
+      str.erase(found,1);
+    }
+  }
   void exclude_col(size_t pos, std::string& str){
     for(size_t i = pos%9;i<board.size();i+=9){
-      str.erase(str.find(board[i]));
+      exclude(board[i],str);
     }
   }
   void exclude_row(size_t pos, std::string& str){
     for(size_t i = 0;i<9;i++){
-      str.erase(str.find(board[i+pos/9]));
+      exclude(board[i+pos/9],str);
     }
   }
   void exclude_box(size_t pos, std::string& str){
-    //TODO
+    size_t box_start=(pos - pos%27)+(pos%9-pos%3);
+    for(int i=0;i<3;i++){
+      for(int j=0;j<3;j++){
+	exclude(board[box_start+i*27+j],str);
+      }
+    }
   }
   void exclude_invalids(size_t pos, std::string& str){
     exclude_col(pos,str);
@@ -38,6 +51,7 @@ class sudoku{
 	e.location=i;
 	e.valid_values="123456789";
 	exclude_invalids(i,e.valid_values);
+	//std::cout<<"pos "<<i<<" "<<e.valid_values<<std::endl;
 	if(e.valid_values.empty()){
 	  return false;
 	}
@@ -47,6 +61,20 @@ class sudoku{
     if(empties.empty()){
       return true;
     }
+    //find smallest empty
+    empty smallest=empties[0];
+    for(size_t i=1;i<empties.size();i++){
+      if(empties[i].valid_values.size() < smallest.valid_values.size()){
+	smallest=empties[i];
+      }
+    }
+    for(size_t i =0 ;i < smallest.valid_values.size() ;i++){
+      board[smallest.location]=smallest.valid_values[i];
+      if(solve()){
+	return true;
+      }
+    }
+    board[smallest.location]='0';
     return false;
   }
   const std::string get_solution(){
