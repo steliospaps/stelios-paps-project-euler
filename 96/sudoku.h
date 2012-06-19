07184b556b98ca28cce3b80a1b15ba3d76e3a582
stelios paps <stelios05@gmail.com>
2012-06-19 21:10:25 +0100

    tests pass

--- 96/sudoku.h
+++ 96/sudoku.h
@@ -16,33 +16,33 @@ class sudoku{
   void exclude(char c,std::string& str){
     size_t found=str.find(c);
     if(found!=std::string::npos){
-      std::cout<<"   removing "<<c<<" from "<<str<<std::endl;
+      //std::cout<<"   removing "<<c<<" from "<<str<<std::endl;
       str.erase(found,1);
     }
   }
   void exclude_col(size_t pos, std::string& str){
     for(size_t i = pos%9;i<board.size();i+=9){
-      std::cout<<"  excluding col "<<pos<<" "<<i<<std::endl;
+      //std::cout<<"  excluding col "<<pos<<" "<<i<<std::endl;
       exclude(board[i],str);
     }
   }
   void exclude_row(size_t pos, std::string& str){
     for(size_t i = 0;i<9;i++){
-      std::cout<<"  excluding row "<<pos<<" "<<i<<std::endl;
-      exclude(board[i+pos/9],str);
+      //std::cout<<"  excluding row "<<pos<<" "<<i+pos/9<<std::endl;
+      exclude(board[i+pos-pos%9],str);
     }
   }
   void exclude_box(size_t pos, std::string& str){
     size_t box_start=(pos - pos%27)+(pos%9-pos%3);
     for(int i=0;i<3;i++){
       for(int j=0;j<3;j++){
-	std::cout<<"  excluding box "<<pos<<" "<<i<<" "<<j<<" "<<box_start+i*9+j<<std::endl;
+	//std::cout<<"  excluding box "<<pos<<" "<<i<<" "<<j<<" "<<box_start+i*9+j<<std::endl;
 	exclude(board[box_start+i*9+j],str);
       }
     }
   }
   void exclude_invalids(size_t pos, std::string& str){
-    std::cout<<" excluding invalids for "<<pos<<std::endl;
+    //std::cout<<" excluding invalids for "<<pos<<std::endl;
     exclude_col(pos,str);
     exclude_row(pos,str);
     exclude_box(pos,str);
@@ -57,7 +57,7 @@ class sudoku{
 	exclude_invalids(i,e.valid_values);
 	//std::cout<<"pos "<<i<<" "<<e.valid_values<<std::endl;
 	if(e.valid_values.empty()) {
-	  std::cout<<" nothing found for pos "<<i<<" failing this path"<<std::endl;
+	  //std::cout<<" nothing found for pos "<<i<<" failing this path"<<std::endl;
 	  return false;
 	}
 	empties.push_back(e);
@@ -73,11 +73,11 @@ class sudoku{
 	smallest=empties[i];
       }
     }
-    std::cout<<"investigating "<<smallest.location<<" "<<smallest.valid_values<<std::endl;
+    //std::cout<<"investigating "<<smallest.location<<" "<<smallest.valid_values<<std::endl;
     for(size_t i =0 ;i < smallest.valid_values.size() ;i++){
       board[smallest.location]=smallest.valid_values[i];
-      std::cout<<" setting "<<smallest.location<<" "<<smallest.valid_values[i]<<std::endl;
-      std::cout<<board<<std::endl;
+      //std::cout<<" setting "<<smallest.location<<" "<<smallest.valid_values[i]<<std::endl;
+      //std::cout<<board<<std::endl;
       if(solve()){
 	return true;
       }
