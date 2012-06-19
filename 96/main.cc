91cb77e8bd20949bc5a502fe90f2e3222304e119
stelios paps <stelios05@gmail.com>
2012-06-19 21:26:52 +0100

    works

--- 96/main.cc
+++ 96/main.cc
@@ -1,1 +1,34 @@
 #include <fstream>
+#include <iostream>
+#include <sstream>
+#include "sudoku.h"
+using namespace std;
+int main(){
+  ifstream in("sudoku.txt");
+  string tmp;
+  int sum=0;
+  while(in>>tmp){
+    //got grid
+    in>>tmp; //number
+    cout<<tmp<<endl;
+    string board;
+    for(int i=0;i<9;i++){
+      in>>tmp;
+      board+=tmp;
+    }
+    cout<<board<<endl;
+    sudoku s(board);
+    if(!s.solve()){
+      cout<<"failed to solve"<<endl;
+      return 1;
+    }
+    cout<<s.get_solution()<<endl;
+    istringstream num(s.get_solution().substr(0,3));
+    int tmp_num;
+    num>>tmp_num;
+    cout<<tmp_num<<endl;
+    sum+=tmp_num;
+  }
+  cout<<sum<<endl;
+  return 0;
+}
