12f293888199f506af16f73be4c062fbb1604937
stelios <stelios05@gmail.com>
2015-10-24 21:16:47 +0100

    103 still too slow (dont even know if it works)

--- 103/scala-103/src/main/scala/Solve.scala
+++ 103/scala-103/src/main/scala/Solve.scala
@@ -1,5 +1,61 @@
-package euler.p103
+
+
+import scala.annotation.tailrec
 
 object Solve extends App {
-
+  def isBad(a:List[Int]): Boolean = {
+  
+  val bad = for(
+              x <- a.permutations.toList;
+              i <- 1 until x.length;
+            (l,r) = x.splitAt(i);//looking for sets in r that have the same sum as l// or are >l with less items
+            r1 <- r.permutations.toList;
+            j <- 1 to r1.length;
+            r2 = r1.take(j)
+            if l.sum == r2.sum || (l.length>r2.length && l.sum < r2.sum)
+            ) yield {
+                  //println(a +":"+(l,r2))
+                  (l,r2)              
+            }
+    !bad.isEmpty
+  }
+  def solve(n:Int):List[Int]={
+    def search(soFar:List[Int],from:Int,tooBig:Int):Option[List[Int]] = {
+      //println(n+" "+soFar+" "+from+" "+tooBig)
+      if(soFar.length == 1) println(n+" "+soFar+" "+from+" "+tooBig)
+      if(soFar.length == n) {
+        if(isBad(soFar)) None
+        else Some(soFar)
+      } else {        
+        val attemps = for(i<- Stream.range(from,tooBig)) yield {
+            val bigest = if(soFar.isEmpty) tooBig else if(soFar.length==1) soFar.sum+i else soFar.take(2).sum
+            //println(" "+i+" "+bigest)
+            if(i<bigest) search(soFar:+i,i+1,bigest)
+            else None
+          }
+        attemps.find(_.isDefined).getOrElse(None)
+      }
+    }
+    search(Nil,1,20).get
+  }
+  def solve2(n:Int):List[List[Int]]={
+    def search2(soFar:List[Int],from:Int,tooBig:Int):Stream[Option[List[Int]]] = {
+      //println(n+" "+soFar+" "+from+" "+tooBig)
+      if(soFar.length == 1) println(n+" "+soFar+" "+from+" "+tooBig)
+      if(soFar.length == n) {
+        if(isBad(soFar)) Stream.Empty
+        else Stream(Some(soFar))
+      } else {        
+        val attemps = for(i<- Stream.range(from,tooBig)) yield {
+            val bigest = if(soFar.isEmpty) tooBig else if(soFar.length==1) soFar.sum+i else soFar.take(2).sum
+            //println(" "+i+" "+bigest)
+            if(i<bigest) search2(soFar:+i,i+1,bigest)
+            else Stream.empty
+          }
+        attemps.flatten
+      }
+    }
+    search2(Nil,19,30).filter({_.isDefined}).map { _.get}.toList
+  }
+  println(solve2(7).foldLeft(List(Int.MaxValue))((a,b)=>if(a.sum<b.sum) a else b))
 }
--- 103/scala-103/src/test/scala/SolveSpec.scala
+++ 103/scala-103/src/test/scala/SolveSpec.scala
@@ -1,14 +1,33 @@
 import org.scalatest.FlatSpec
+import org.scalatest.Matchers
 
-class SolveSpec extends FlatSpec {
+class SolveSpec extends FlatSpec with Matchers{
 
-  "disjoint subsets" should "not be equal" in {
-    assert(Set.empty.size == 0)
+  "Solve" should "return values given by problem description" in {  
+    Solve.solve(1) should contain only (1)
+ 
+    Solve.solve(2) should contain only (1,2)
+    
+    Solve.solve(3) should contain only (2,3,4)
+    
+    Solve.solve(4) should contain only (3,5,6,7)
+    
+    Solve.solve(5) should contain only (6,9,11,12,13)
+    
+    Solve.solve(6) should contain only (11, 18, 19, 20, 22, 25)
   }
-
-  it should "produce NoSuchElementException when head is invoked" in {
-    intercept[NoSuchElementException] {
-      Set.empty.head
-    }
+  
+  "Solve.containsEqualSubsets" should "work correctly" in {
+    Solve.isBad(List(1)) should be (false)
+    Solve.isBad(List(1,2)) should be (false)
+    Solve.isBad(List(1,2,3)) should be (true)
+    Solve.isBad(List(1,2,3,4)) should be (true)
+    Solve.isBad(List(2,3,4)) should be (false)
+    Solve.isBad(List(3,5,6,7)) should be (false)
+    Solve.isBad(List(6,9,11,12,13)) should be (false)
+    Solve.isBad(List(11, 18, 19, 20, 22, 25)) should be (false)
+    Solve.isBad(List(11, 17, 20, 22, 23, 24)) should be (false)
   }
+    
+ 
 }
